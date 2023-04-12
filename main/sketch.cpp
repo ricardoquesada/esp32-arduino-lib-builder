ebb0cfee10b9404b98e36281c1811ae7f55392b8
Ricardo Quesada <ricardoquesada@gmail.com>
2023-04-11 18:14:50 -0700

    updated sketch to use Serial

--- main/sketch.cpp
+++ main/sketch.cpp
@@ -1,42 +1,5 @@
-/****************************************************************************
-http://retro.moe/unijoysticle2
-
-Copyright 2021 Ricardo Quesada
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-****************************************************************************/
-
-#include "sdkconfig.h"
-#ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
-#error "Must only be compiled when using Bluepad32 Arduino platform"
-#endif  // !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
-
 #include <Arduino.h>
 #include <Bluepad32.h>
-
-//
-// README FIRST, README FIRST, README FIRST
-//
-// Bluepad32 has a built-in interactive console.
-// By default it is enabled (hey, this is a great feature!).
-// But it is incompatible with Arduino "Serial" class.
-//
-// Instead of using "Serial" you can use Bluepad32 "Console" class instead.
-// It is somewhat similar to Serial but not exactly the same.
-//
-// Should you want to still use "Serial", you have to disable the Bluepad32's console
-// from "sdkconfig.defaults" with:
-//    CONFIG_BLUEPAD32_USB_CONSOLE_ENABLE=n
 
 GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
 
@@ -46,11 +9,11 @@ void onConnectedGamepad(GamepadPtr gp) {
     bool foundEmptySlot = false;
     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
         if (myGamepads[i] == nullptr) {
-            Console.printf("CALLBACK: Gamepad is connected, index=%d\n", i);
+            Serial.printf("CALLBACK: Gamepad is connected, index=%d\n", i);
             // Additionally, you can get certain gamepad properties like:
             // Model, VID, PID, BTAddr, flags, etc.
             GamepadProperties properties = gp->getProperties();
-            Console.printf("Gamepad model: %s, VID=0x%04x, PID=0x%04x\n", gp->getModelName(), properties.vendor_id,
+            Serial.printf("Gamepad model: %s, VID=0x%04x, PID=0x%04x\n", gp->getModelName().c_str(), properties.vendor_id,
                            properties.product_id);
             myGamepads[i] = gp;
             foundEmptySlot = true;
@@ -58,7 +21,7 @@ void onConnectedGamepad(GamepadPtr gp) {
         }
     }
     if (!foundEmptySlot) {
-        Console.println("CALLBACK: Gamepad connected, but could not found empty slot");
+        Serial.println("CALLBACK: Gamepad connected, but could not found empty slot");
     }
 }
 
@@ -67,7 +30,7 @@ void onDisconnectedGamepad(GamepadPtr gp) {
 
     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
         if (myGamepads[i] == gp) {
-            Console.printf("CALLBACK: Gamepad is disconnected from index=%d\n", i);
+            Serial.printf("CALLBACK: Gamepad is disconnected from index=%d\n", i);
             myGamepads[i] = nullptr;
             foundGamepad = true;
             break;
@@ -75,13 +38,14 @@ void onDisconnectedGamepad(GamepadPtr gp) {
     }
 
     if (!foundGamepad) {
-        Console.println("CALLBACK: Gamepad disconnected, but not found in myGamepads");
+        Serial.println("CALLBACK: Gamepad disconnected, but not found in myGamepads");
     }
 }
 
 // Arduino setup function. Runs in CPU 1
 void setup() {
-    Console.printf("Firmware: %s\n", BP32.firmwareVersion());
+    Serial.begin(115200);
+    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
 
     // Setup the Bluepad32 callbacks
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
@@ -155,7 +119,7 @@ void loop() {
             // Another way to query the buttons, is by calling buttons(), or
             // miscButtons() which return a bitmask.
             // Some gamepads also have DPAD, axis and more.
-            Console.printf(
+            Serial.printf(
                 "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, "
                 "%4d, brake: %4d, throttle: %4d, misc: 0x%02x\n",
                 i,                        // Gamepad Index
