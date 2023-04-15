6f23ee115ff1861a3ee6d6a01bb4fa2c817bdd0e
Ricardo Quesada <ricardoquesada@gmail.com>
2023-04-15 07:04:31 -0700

    updating example

--- main/sketch.cpp
+++ main/sketch.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <Bluepad32.h>
 
 GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
@@ -139,5 +138,12 @@ void loop() {
         }
     }
 
+    // The main loop must have some kind of "yield to lower priority task" event.
+    // Otherwise the watchdog will get triggered.
+    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+    // Detailed info here:
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+
+    // vTaskDelay(1);
     delay(150);
 }
