7171e44b3166aef7c86006547fdea5e833441ded
Ricardo Quesada <ricardoquesada@gmail.com>
2023-04-17 21:38:26 -0700

    from Bluepad32 v3.7.0

--- bluepad32_files/libraries/Bluepad32_ESP32/examples/Controller/Controller.ino
+++ bluepad32_files/libraries/Bluepad32_ESP32/examples/Controller/Controller.ino
@@ -45,6 +45,8 @@ void onDisconnectedGamepad(GamepadPtr gp) {
 void setup() {
     Serial.begin(115200);
     Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+    const uint8_t* addr = BP32.localBdAddress();
+    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
 
     // Setup the Bluepad32 callbacks
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
